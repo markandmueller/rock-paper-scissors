0022520ccb717cb96c07083534249dd1f483f040
markandmueller <markamueller@mac.com>
2022-11-12 15:38:55 -0800

    playRound improved if statements

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// [] create a Rock, Paper, Scissors game in the browser console that:
-
+// [] create a Rock, Paper, Scissors game in the browser console
 // [x] generates a random computer selection of rock, paper, or scissors
 let choicesArray = ['rock', 'paper', 'scissors'];
 function getComputerChoice(choicesArray) {
@@ -15,42 +14,41 @@ const playerSelection = "rock";
 console.log("Player Selection:", playerSelection);
 console.log("Computer Selection:", computerSelection);
 
-// [] write a function that plays a single round of Rock Paper Scissors
-// [] at end of round, return a string that declares the winner of the round
-// [] like this: "You lose! Paper beats Rock"
-// [] want to return the results of this function call and not console.log() them 
-// [] because we're going to use the return values later on
-
-// function areDifferent(playerSelection, computerSelection) {
-//     if (playerSelection !== computerSelection) {
-//         // console.log('look, we are different');
-//         return 'look, we are DIFFERENT';
-//     } if (playerSelection == computerSelection) {
-//         // console.log('look, we are the SAME');
-//         return 'look, we are the SAME';
-//     }
-// } console.log(areDifferent(playerSelection, computerSelection));
-
+// [x] write a function that plays a single round of Rock Paper Scissors
+// improve playRound to have 4 statements for if tie, if win, if lose, if else
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        return 'You Win! Rock beats Scissors.';
-    } if (playerSelection == "rock" && computerSelection == "paper") {
-        return 'You Lose! Paper beats Rock.';
-    } if (playerSelection == "paper" && computerSelection == "rock") {
-        return 'You Win! Paper beats Rock.';
-    } if (playerSelection == "paper" && computerSelection == "scissors") {
-        return 'You Lose! Scissors beats Paper.';
-    } if (playerSelection == "scissors" && computerSelection == "paper") {
-        return 'You Win! Scissors beats Paper.';
-    } if (playerSelection == "scissors" && computerSelection == "rock") {
-        return 'You Lose! Scissors beats Rock.';
-    } if (playerSelection == computerSelection) {
+    if (playerSelection == computerSelection) {
         return 'It is a TIE!';
-    }
+    } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`;
+    } else if (
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "rock")) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    } else { return 'Whoops! Something is most definitely wrong.' }
 }
+
 console.log(playRound(playerSelection, computerSelection));
 
+// let playerScore = 0;
+// let computerScore = 0;
+// let score = (playerScore, computerScore);
 
-// [] write a new function called game()
-//         [] call the playRound function inside of this to play a 5 round game that
-//             [] keeps score and reports a winner or loser at the end //
+// // [] write a new function called game()
+// // function game() {
+// //     for (let i = 0; i < 5; i++) {
+// //         playRound(playerSelection, computerSelection);
+// //         return playRound(playerSelection, computerSelection);
+// //     }
+// // }
+// // call the playRound function inside of this to play a 5 round game that
+// // keeps score and reports a winner or loser at the end
+
+// // console.log(game);
+// console.log("Computer Score:", computerScore);
+// console.log("Player Score:", playerScore);
+// console.log("Total Score:", score);
