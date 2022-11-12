53b2ce5e3b1b88cbec9bc2d05f8dbb40549f2169
markandmueller <markamueller@mac.com>
2022-11-12 13:48:14 -0800

    playRound function successful

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let choicesArray = ['rock', 'paper', 'scissors'];
 let randomComputerChoice = choicesArray[Math.floor(Math.random() * choicesArray.length)];
 let playerInput = prompt('Type in rock, paper, or scissors');
@@ -33,10 +34,68 @@ playRound(playerSelection, computerSelection);
 
 console.log("Player Selected:", playerSelection);
 console.log("Computer Selected:", computerSelection);
+=======
+// [] create a Rock, Paper, Scissors game in the browser console that:
 
-// console.log(playRound(playerSelection, computerSelection));
+// [x] generates a random computer selection of rock, paper, or scissors
+let choicesArray = ['rock', 'paper', 'scissors'];
+function getComputerChoice(choicesArray) {
+    return choicesArray[Math.floor(Math.random() * choicesArray.length)];
+}
+const computerSelection = getComputerChoice(choicesArray);
+
+// [x] prompts the user for their Selection of rock, paper, or scissors
+const playerSelection = "rock";
+// let playerInput = prompt('Type in rock, paper, or scissors');
+// const playerSelection = playerInput.toLowerCase();
+
+console.log("Player Selection:", playerSelection);
+console.log("Computer Selection:", computerSelection);
+
+// [] write a function that plays a single round of Rock Paper Scissors
+// [] at end of round, return a string that declares the winner of the round
+// [] like this: "You lose! Paper beats Rock"
+// [] want to return the results of this function call and not console.log() them 
+// [] because we're going to use the return values later on
+
+// function areDifferent(playerSelection, computerSelection) {
+//     if (playerSelection !== computerSelection) {
+//         // console.log('look, we are different');
+//         return 'look, we are DIFFERENT';
+//     } if (playerSelection == computerSelection) {
+//         // console.log('look, we are the SAME');
+//         return 'look, we are the SAME';
+//     }
+// } console.log(areDifferent(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return 'You Win! Rock beats Scissors.';
+    } if (playerSelection == "rock" && computerSelection == "paper") {
+        return 'You Lose! Paper beats Rock.';
+    } if (playerSelection == "paper" && computerSelection == "rock") {
+        return 'You Win! Paper beats Rock.';
+    } if (playerSelection == "paper" && computerSelection == "scissors") {
+        return 'You Lose! Scissors beats Paper.';
+    } if (playerSelection == "scissors" && computerSelection == "paper") {
+        return 'You Win! Scissors beats Paper.';
+    } if (playerSelection == "scissors" && computerSelection == "rock") {
+        return 'You Lose! Scissors beats Rock.';
+    } if (playerSelection == computerSelection) {
+        return 'It is a TIE!';
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
+>>>>>>> Stashed changes
+
+
+<<<<<<< Updated upstream
 
 
 
 
-
+=======
+// [] write a new function called game()
+//         [] call the playRound function inside of this to play a 5 round game that
+//             [] keeps score and reports a winner or loser at the end //
+>>>>>>> Stashed changes
