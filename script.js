a6150f2bec11b1df68da63faa1ce45bcbec1db6a
markandmueller <markamueller@mac.com>
2022-12-07 18:08:00 -0800

    working properly but want to improve code and add style

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
-let playerSelection = undefined;
-
 const p1ScoreHolder = document.querySelector('#p1Score');
 const compScoreHolder = document.querySelector('#compScore');
 const compChoiceDisplay = document.querySelector('#compChoiceDisplay');
-let roundMessage = document.querySelector('#roundMessage');
+const roundMessage = document.querySelector('#roundMessage');
+const resetBtnContainer = document.querySelector('#resetBtnContainer');
+const resetButton = document.querySelector('#reset');
+resetButton.remove();
 
-const rock = document.querySelector('#rock');
-rock.addEventListener('click', e => {
-    playerSelection = rock.value;
-    game();
-    // playRound(playerSelection, computerSelection);
-})
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', e => {
-    playerSelection = paper.value;
-    game();
-})
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', e => {
-    playerSelection = scissors.value;
-    game();
+// Player 1 RPS buttons logic
+const btnContainer = document.querySelector('#rpsButtonsContainer');
+const rpsButtons = btnContainer.querySelectorAll('button');
+rpsButtons.forEach((button) => {
+    button.addEventListener('click', function () {
+        playerSelection = button.value;
+        game();
+    });
 })
 
 function game() {
@@ -35,45 +29,62 @@ function game() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    roundMessage.innerText = 'we played a round';
     compChoiceDisplay.innerText = computerSelection;
     if (playerSelection == computerSelection) {
         drawScore = drawScore += 1;
-        console.log('It is a TIE!');
-        gameOver();
+        roundMessage.innerText = `You both chose ${playerSelection}`;
+        isGameOver();
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")) {
         playerScore = playerScore += 1;
         p1ScoreHolder.innerText = playerScore;
-        console.log(`${playerSelection} beats ${computerSelection}.`);
-        gameOver();
-    } else if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock")) {
+        roundMessage.innerText = `${playerSelection} beats ${computerSelection}.`;
+        isGameOver();
+    } else {
         computerScore = computerScore += 1;
         compScoreHolder.innerText = computerScore;
-        console.log(`${computerSelection} beats ${playerSelection}.`);
-        gameOver();
-    } else if (
-        (playerSelection !== "rock" || "paper" || "scissors")) {
-        console.log('Try choosing rock, paper, or scissors.');
-        gameOver();
-    } else {
-        console.log('Whoops! Something is most definitely wrong.');
-        gameOver();
+        roundMessage.innerText = `${computerSelection} beats ${playerSelection}.`;
+        isGameOver();
     }
 }
 
-function gameOver() {
-    // consider adding a dropdown for PLAY TO #?
-    if ((playerScore === 5) || (computerScore === 5)) {
-        if (playerScore > computerScore) {
-            console.log("You WON the match!");
-        } else if (playerScore < computerScore) {
-            console.log("You LOST the match!");
-        } else console.log("You TIED.");
+function isGameOver() {
+    if ((playerScore === 3) || (computerScore === 3)) {
+        matchOverMsg();
     }
+}
+
+// add function to disable buttons when Match is Over
+function matchOverMsg() {
+    const matchWinner = document.querySelector('#matchWinner');
+    rpsButtons.forEach((button) => {
+        button.disabled = true;
+    });
+    showResetButton();
+    if (playerScore > computerScore) {
+        matchWinner.innerText = "You WON the match!";
+    } else (playerScore < computerScore)
+    matchWinner.innerText = "You LOST the match!";
+}
+
+function showResetButton() {
+    resetBtnContainer.appendChild(resetButton);
+    resetButton.addEventListener('click', playAgain);
+}
+
+function playAgain() {
+    playerScore = 0;
+    computerScore = 0;
+    drawScore = 0;
+    resetButton.remove();
+    rpsButtons.forEach((button) => {
+        button.disabled = false;
+    });
+    matchWinner.innerText = "Let's play again.";
+    p1ScoreHolder.innerText = playerScore;
+    compScoreHolder.innerText = computerScore;
+    compChoiceDisplay.innerText = "";
+    roundMessage.innerText = "";
 }
